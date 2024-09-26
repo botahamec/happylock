7443474f1f00d2a9306079641dbb3c18df5e6445
Botahamec <botahamec@outlook.com>
2024-09-25 20:59:09 -0400

    Reorganize PoisonFlag methods

--- src/poisonable/flag.rs
+++ src/poisonable/flag.rs
@@ -1,35 +1,33 @@
-use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::Relaxed;
+#[cfg(panic = "unwind")]
+use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
 
 use super::PoisonFlag;
 
+#[cfg(panic = "unwind")]
 impl PoisonFlag {
-	#[cfg(panic = "unwind")]
 	pub const fn new() -> Self {
 		Self(AtomicBool::new(false))
 	}
 
-	#[cfg(not(panic = "unwind"))]
-	pub const fn new() -> Self {
-		Self()
-	}
-
-	#[cfg(panic = "unwind")]
 	pub fn is_poisoned(&self) -> bool {
 		self.0.load(Relaxed)
 	}
 
-	#[cfg(not(panic = "unwind"))]
+	pub fn clear_poison(&self) {
+		self.0.store(true, Relaxed)
+	}
+}
+
+#[cfg(not(panic = "unwind"))]
+impl PoisonFlag {
+	pub const fn new() -> Self {
+		Self()
+	}
+
 	pub fn is_poisoned(&self) -> bool {
 		false
 	}
 
-	#[cfg(panic = "unwind")]
-	pub fn clear_poison(&self) {
-		self.0.store(true, Relaxed)
-	}
-
-	#[cfg(not(panic = "unwind"))]
 	pub fn clear_poison(&self) {
 		()
 	}
